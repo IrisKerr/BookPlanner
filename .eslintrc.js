79746f6c8b23f8c9a25285afcc13933ee4d0c5ce
IrisKerr <iris.kerr@hotmail.com>
2024-07-04 19:36:38 +0200

    Login View add + prettier/eslint additional config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,13 @@ module.exports = {
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: true,
   },
+  plugins: ["prettier"],
+  parser: "vue-eslint-parser",
   rules: {
+    "prettier/prettier": "error",
+    "vue/multi-word-component-names": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <form action="">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+}
+</script>
+
+<style></style>
