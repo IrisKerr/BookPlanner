7b316440b31f91502d702604073bb4cef9f1bbef
IrisKerr <iris.kerr@hotmail.com>
2024-07-09 16:17:46 +0200

    🔧firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+dotenv.config()
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
