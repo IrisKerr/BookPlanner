8dd1568783f564fbcb3b1b6bad710012fda964de
IrisKerr <iris.kerr@hotmail.com>
2024-08-22 17:38:33 +0200

    ✨ conditional rendering login/logout buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,9 +6,13 @@
         <router-link :to="{ name: 'Home' }"> Book planner </router-link>
       </h1>
       <div class="links">
-        <button @click="handleClick">Logout</button>
-        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
-        <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        <div v-if="user">
+          <button @click="handleClick">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </div>
       </div>
     </nav>
   </div>
@@ -16,13 +20,16 @@
 
 <script>
 import useLogout from '@/composables/useLogout'
+import getUser from '@/composables/getUser'
 import { useRouter } from 'vue-router'
 
 export default {
   name: 'Navbar',
   setup() {
+    const { user } = getUser()
     const { logout } = useLogout()
     const router = useRouter()
+    console.log('User state:', user)
 
     const handleClick = async () => {
       await logout()
@@ -30,7 +37,7 @@ export default {
       router.push({ name: 'Login' })
     }
 
-    return { handleClick }
+    return { handleClick, user }
   },
 }
 </script>
