034d5fbbf7a805f783c52bfba7a1bdd7028ee21c
IrisKerr <iris.kerr@hotmail.com>
2024-08-23 20:49:34 +0200

    ✨Create Booklist component

---
+++ src/views/booklists/CreateBooklist.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h4>Create New Booklist</h4>
+    <input type="text" required placeholder="Booklist Title" v-model="title" />
+    <textarea
+      required
+      placeholder="Booklist Description..."
+      v-model="description"
+    ></textarea>
+    <!-- Upload booklist image -->
+    <label>Upload Booklist cover image</label>
+    <input type="file" />
+    <button>Create</button>
+    <div class="error"></div>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const title = ref('')
+    const description = ref('')
+
+    const handleSubmit = () => {
+      console.log(title.value, description.value)
+    }
+    return {
+      title,
+      description,
+      handleSubmit,
+      error,
+    }
+  },
+}
+</script>
+
+<style></style>
