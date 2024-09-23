6b0501e54d3c3766b188a03d281ae9c3018aee1f
Iris Kerr <iris.kerr@hotmail.com>
2024-09-23 19:28:22 +0000

    🥅useStorage getUser

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -2,22 +2,33 @@ import { projectStorage } from '@/firebase/config'
 import { ref } from 'firebase/storage'
 import getUser from './getUser'
 
-const user = getUser()
+const { user } = getUser() 
 
 const useStorage = () => {
+
   const error = ref(null)
   const url = ref(null)
   const filePath = ref(null)
 
   const uploadImage = async (file) => {
+  
+    if (!user || !user.value) {
+      error.value = "user is not connected"
+      console.log(error.value)
+      return
+    }
+
+
     filePath.value = `covers/${user.value.uid}/${file.name}`
     const storageRef = projectStorage.ref(filePath.value)
 
     try {
       const res = await storageRef.put(file)
       url.value = await res.ref.getDownloadURL()
+      console.log('storage updated')
     } catch (error) {
       error.value = error.message
+      console.log(error.value)
     }
   }
 
--- src/views/booklists/CreateBooklist.vue
+++ src/views/booklists/CreateBooklist.vue
@@ -17,8 +17,11 @@
 
 <script>
 import { ref } from 'vue'
+import useStorage from '@/composables/useStorage'
+
 export default {
   setup() {
+    const { filePath, url, uploadImage } = useStorage()
     const title = ref('')
     const description = ref('')
     const file = ref(null)
@@ -27,9 +30,10 @@ export default {
     // allow file types
     const types = ['image/png', 'image/jpeg']
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
       if (file.value) {
-        console.log(title.value, description.value, file.value)
+        await uploadImage(file.value)
+        console.log('image uploaded, url: ', url.value)
       }
     }
 
