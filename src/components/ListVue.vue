f8bebe7174426613c2593d659342969b32b3ba49
IrisKerr <iris.kerr@hotmail.com>
2024-10-09 20:18:32 +0200

    ✨List component add

---
+++ src/components/ListVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="booklist in booklists" :key="booklist.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="booklist.coverUrl" />
+      </div>
+      <div class="info">
+        <h3>{{ booklist.title }}</h3>
+        <p>created by {{ booklist.userName }}</p>
+      </div>
+      <div class="booklist-number">
+        <p>{{ booklist.book.length }} books</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['booklists'],
+}
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <div v-if="error" class="error">Could not fetch the data</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <ListView :booklists="documents" />
     </div>
   </div>
 </template>
 
 <script>
 import getCollection from '@/composables/getCollection'
+import ListView from '../components/ListView.vue'
 import { watch } from 'vue'
 
 export default {
   name: 'HomeView',
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection('booklists')
     watch(documents, (newDocs) => {
