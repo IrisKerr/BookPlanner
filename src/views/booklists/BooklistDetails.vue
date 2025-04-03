8684f8bf82740feb3d4a762b7f088f8f9d640af5
IrisKerr <iris.kerr@hotmail.com>
2025-04-03 21:40:57 +0200

    Booklist details

--- src/views/booklists/BooklistDetails.vue
+++ src/views/booklists/BooklistDetails.vue
@@ -1,10 +1,27 @@
 <template>
-  <h2>Playlist details. ID - {{ id }}</h2>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="document" class="booklist-details">
+    <div class="booklist-info">
+      <div class="book-cover">
+        <img .src="document.coverUrl" alt="" />
+      </div>
+      <h2>{{ document.title }}</h2>
+      <p class="username">Created by {{ booklist.userName }}</p>
+      <p class="description">{{ document.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import getDocument from '@/composables/getDocument'
+
 export default {
   props: ['id'],
+  setup(props) {
+    const { error, document } = getDocument('booklist', props.id)
+    console.log(document)
+    return { error, document }
+  },
 }
 </script>
 
